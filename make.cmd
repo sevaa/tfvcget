43b748289da6cb70f8aba9a85f2c0a43766767dc
Seva Alekseyev <sevaa@nih.gov>
2024-07-11 11:09:14 -0400

    NPM dep

--- make.cmd
+++ make.cmd
@@ -2,5 +2,7 @@ rem run with /b to prevent automatic version bumping
 
 del *.vsix
 rem The Powershell files should be in path!!!
-if not x%1 == x/b powershell BumpTaskVersion.ps1 -Folder src\TFVCGet & powershell BumpExtVersion.ps1 -File src\ext.json
-call %APPDATA%\npm\tfx extension create --manifest-globs ext.json --root src
+if not x%1 == x/b set OPT=--rev-version
+if not exist %APPDATA%\npm\azp-task-bump.cmd npm i -g azp-task-bump
+if not x%1 == x/b azp-task-bump src\TFVCGet
+call %APPDATA%\npm\tfx extension create %OPT% --manifest-globs ext.json --root src
