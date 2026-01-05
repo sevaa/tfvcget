3094bc82811fc1dec39072ebab5b818b9c9eb555
Seva Alekseyev <sevaa@nih.gov>
2026-01-05 10:50:06 -0500

    Make fix

--- make.cmd
+++ make.cmd
@@ -5,5 +5,5 @@ rem The Powershell files should be in path!!!
 if not x%1 == x/b set OPT=--rev-version
 if not exist %APPDATA%\npm\azp-task-bump.cmd npm i -g azp-task-bump
 if not exist %APPDATA%\npm\tfx.cmd echo No TFX!!! & goto EOF
-if not x%1 == x/b azp-task-bump src\TFVCGet
-call %APPDATA%\npm\tfx.cmd extension create %OPT% --manifest-globs ext.json --root src
+if not x%1 == x/b call azp-task-bump src\TFVCGet
+call %APPDATA%\npm\tfx extension create %OPT% --manifest-globs ext.json --root src
